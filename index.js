af1d6f0ffb8ceb773c14faf92844a499c24c360c
RIEAD <mdriead.bd@gmail.com>
2024-06-04 03:19:47 +0600

    Single camp data read and update functionality added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -10,7 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cricab9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -25,29 +25,99 @@ async function run() {
   try {
     await client.connect();
 
-     // DB Collections
-     const campsCollection = client.db("medcamp").collection("camps")
+    // DB Collections
+    const campsCollection = client.db("medcamp").collection("camps")
+    const organizerCollection = client.db("medcamp").collection("organizer")
 
-     // JWT related api
-     app.post('/jwt', async (req, res) => {
-       const user = req.body;
-       const token = jwt.sign(user, process.env.SECRET_TOKEN, { expiresIn: '12h' });
-       res.send({ token });
-     })
-     
-     //Get all camps from db  
-     app.get('/camps', async(req,res)=>{
-        const result = await campsCollection.find().toArray()
-        res.send(result)
-     })
+    // Verify Token
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'Unauthorized Access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'Unauthorized Access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+    // Verify Organizer
+    const verifyOrganizer = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await organizerCollection.findOne(query);
+      if (!user) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+
+    // Check Organizer
+    app.get('/organizer/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      const user = await organizerCollection.findOne({email});
+      let organizer = false;
+      if (user) {
+        organizer = true;
+      }
+      res.send({ organizer });
+    })
+
+    // JWT related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.SECRET_TOKEN, { expiresIn: '12h' });
+      res.send({ token });
+    })
+
+    //Get all camps from db  
+    app.get('/camps', async (req, res) => {
+      const result = await campsCollection.find().toArray()
+      res.send(result)
+    })
+
+    //Get single camp details from db  
+    app.get('/camp/details/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await campsCollection.findOne(query)
+      res.send(result)
+    })
 
     //Save a camp to db
-    app.post('/camps', async(req, res)=>{
-        const camp = req.body;
-        const result = await campsCollection.insertOne(camp);
-        res.send(result)
-    })  
+    app.post('/camps', verifyToken, verifyOrganizer, async (req, res) => {
+      const camp = req.body;
+      const result = await campsCollection.insertOne(camp);
+      res.send(result)
+    })
 
+    //Update a camp to db
+    app.patch('/camp/:id', async (req, res) => {
+      const id = req.params.id
+      const camp = req.body;
+      const query = {_id : new ObjectId(id)}
+      const updateDoc = {
+        $set : {
+          ...camp
+        }
+      }
+      const result = await campsCollection.updateOne(query, updateDoc);
+      res.send(result)
+    })
+
+    // Get organizer profile data
+    app.get('/organizer-data/:email', async(req, res)=>{
+      const email = req.params.email;
+      const result = await organizerCollection.findOne({email})
+      res.send(result)
+    })
 
 
 
@@ -58,6 +128,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.listen(port, ()=>{
-    console.log("Medcamp Server Is Running On Port : ", port);
+app.listen(port, () => {
+  console.log("Medcamp Server Is Running On Port : ", port);
 })
